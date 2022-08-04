8f8da286fbbe3a00c183c7817d86a0dae78c00e6
03mayank <mdubey@grepruby.io>
2022-08-04 18:29:21 +0530

    Ruby basic -I first commit

---
+++ 3. joining_Arrays_without_inbuilt_methods.rb
@@ -0,0 +1,12 @@
+arr1 = [1,2,3,4,5]
+arr2 = [6,7,8,9,0]
+arr3 = Array.new(arr1.size + arr2.size, 0)
+for i in 0..arr1.size-1 do
+    arr3[i] = arr1[i]
+end
+j = 0
+for i in arr1.size..arr3.size-1 do
+    arr3[i] = arr2[j]
+    j += 1
+end
+puts arr3
