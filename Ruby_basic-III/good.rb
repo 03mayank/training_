67acd9ac8e98755141c015edd85e14349c9b4af8
03mayank <mdubey@grepruby.io>
2022-08-12 17:11:14 +0530

    Handling -ve values

--- Ruby_basic-III/good.rb
+++ Ruby_basic-III/good.rb
@@ -41,7 +41,38 @@ parse(item_list, new_reciept)
 
 puts new_reciept.print_list
 
+# Input 1 =>
+# 1 book at 12.49
+# 1 music CD at 14.99
+# 1 chocolate bar at 0.85
 
+# Output 1 =>
+# 1 book : 12.49
+# 1 music CD : 16.489
+# 1 chocolate bar : 0.85
+# Sales Taxes: 1.5
+# Total: 29.83
 
+# Input 2 =>
+# 1 imported box of chocolates at 10.00
+# 1 imported bottle of perfume at 47.50
+
+# Output 2=>
+# 1 imported box of chocolates : 10.5
+# 1 imported bottle of perfume : 54.625
+# Sales Taxes: 7.63
+# Total: 65.13
+
+# Input 3=>
+# 1 book at 12.49
+# -1 music CD at 14.99
+# 1 chocolate bar at 0.85
+
+# Output
+# 1 book : 12.49
+# Invalid input
+# 1 chocolate bar : 0.85
+# Sales Taxes: 0.0
+# Total: 13.34
 
 
